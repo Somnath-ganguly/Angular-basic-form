ab24a9f16a9e6ccf01ecd60855ad36a55e367804
Somnath Ganguly <145208724+creativeSomnath@users.noreply.github.com>
2024-06-08 01:17:44 +0530

    Angular-basic-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { CalendarModule } from 'primeng/calendar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CalendarModule,
+    BrowserAnimationsModule
   ],
   providers: [
     provideClientHydration()
